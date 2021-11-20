f7feddf99c6f09fbab8ca308a4b63cb4b9a48630
fangshuhong11 <64949285+fangshuhong11@users.noreply.github.com>
2021-11-20 20:02:23 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,16 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'd3e91b3ae4e04489aa372bf5563ee83d',
+
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'personal developer',
+  domain: 'https://checkered-cement-894.notion.site/d3e91b3ae4e04489aa372bf5563ee83d',
+  author: 'Fang Shuhong',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +18,9 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  zhihu: '小甜心',
+  github: 'fangshuhong11',
+  douban: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
